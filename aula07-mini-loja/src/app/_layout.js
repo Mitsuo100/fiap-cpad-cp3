26d0dc108435b14426916a6c5a3d147040cf237e
Mitsuo10 <pedro.risardi@gmail.com>
2026-05-18 14:34:20 -0300

    erros resolvidos em memolist e mini-loja

--- aula07-mini-loja/src/app/_layout.js
+++ aula07-mini-loja/src/app/_layout.js
@@ -1,10 +1,15 @@
 import { Stack } from "expo-router";
+
 import { CartProvider } from "../context/CartContext";
 
 export default function Layout() {
   return (
     <CartProvider>
-      <Stack screenOptions={{ headerShown: false }} />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
     </CartProvider>
   );
 }
